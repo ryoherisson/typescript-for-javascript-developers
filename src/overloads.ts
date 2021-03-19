8787dd794f14aaf831e33e94d104f37e4a482e94
ryoherisson <ryhizw@gmail.com>
2021-03-20 01:12:48 +0900

    overload

---
+++ src/overloads.ts
@@ -0,0 +1,23 @@
+export {};
+
+// signature
+function double(value: number): number;
+function double(value: string): string;
+
+// overloadではany型
+function double(value: any): any {
+    console.log(typeof value);
+    if (typeof value === 'number') {
+        return value * 2;
+    } else {
+        return value + value;
+    }
+}
+
+// no need
+// function double(value: string): string {
+//     return value + value;
+// }
+
+console.log(double(100));
+console.log(double('Go '));
